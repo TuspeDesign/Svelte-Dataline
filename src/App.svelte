2a186791009470105d0467832807f8280c6b1afe
Timo Anttila <info@tuspe.com>
2020-03-31 19:44:30 +0300

    Better references and minified index.html

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,27 @@
 		{
 			title: "Verkkokauppa yritykselle",
 			id: "verkkokaupat",
-			img_desc: "",
+			img: "palvelumme-verkkokaupat",
+			img_desc: "Parempaa verkkokauppaa",
 			desc: "<p>Meille tärkeää on, että pääset nopeasti ja luotettavasti myymään tuotteitasi uudessa verkkokaupassa. Meiltä löytyy kaikki toiminnot, jotka tarvitset menestyvän verkkokaupan ylläpitämiseen ja kehitämme verkkokauppaa paremmaksi yhdessä asiakkaidemme kanssa. Verkkokauppaan on mahdollista myös integroida toiminnanohjausjärjestelmä, kuten esim. Lemonsoft.</p><p>Verkkokaupat teemme helppokäyttöisellä ja selkeällä ProcessWire-verkkokauppa-alustalla. Datalinen kotisivut ja verkkokauppa sopivat kaikille toimialoille ravintoloista vaatekauppoihin ja urheiluseuroista tukkukauppoihin.</p><p>Teemme verkkosivuja, joista olemme ylpeitä!</p>",
 			button: 1,
 			bg: 1
 		}, {
 			title: "Kotisivut urheiluseuralle",
 			id: "urheiluseurat",
-			img_desc: "",
+			img: "palvelumme-urheiluseurat",
+			img_desc: "Kaunis ja helposti päivitettävä kotisivu urheiluseuralle",
 			desc: "<p>Dataline tarjoaa urheiluseuroille monimuotoisen ja helposti päivitettävän urheilualustan. Kattavat perusominaisuudet sisältävät uutiset, joukkuetiedot, videot, kausittaiset ottelut, seuran statiikat, verkkokaupan, aitiovaraukset ja paljon muuta hyödyllistä. Kehitämme sivustoja urheiluseurojen tarpeiden mukaan ja lisäämme vuosittain uusia ominaisuuksia.</p><p>Kaipaako verkkosivut uudistusta? Ota rohkeasti yhteyttä!</p>",
 			button: 0,
 			bg: 2
+		}, {
+			title: "Palveluja alihankintana",
+			id: "kumppanit",
+			img: "uudet-kumppanit",
+			img_desc: "Haemme uusia yhteistyökumppaneita",
+			desc: "<p>Tarvitseeko asiakkaanne verkkosivustoja tai mobiilisovelluksia, mutta se ei ole teidän omaa ydinbisnestänne? Olemme mieluusti kumppaninne, emme kilpailija.</p><p>Tarjoamme mahdollisuutta yhteistyöhön mm. verkkokaupoissa ja urheilussa. Haemme uusia yhteistyökumppaneita, joille voisimme tarjota tuotteitamme ja palveluitamme reilulla kumppanialennuksella. Vahvuuksiamme ovat kokemus, kilpailukykyiset hinnat, nopeat toimitusajat ja erittäin hyvä asiakaspalvelu.</p>",
+			button: 2,
+			bg: 1
 		}
 	]
 	let contact = [
@@ -40,16 +50,19 @@
 	let shop = [
 		{
 			title: "NavyBlue Shop",
+			what: "Verkkokauppa, Lemonsoft",
 			url: "https://navyblue.fi/",
-			img: "processwire-navyblue-2019"
+			img: "processwire-navyblue-2020"
 		}, {
 			title: "Lennol Oy",
+			what: "Verkkokauppa, Lemonsoft",
 			url: "https://www.lennol.fi/",
 			img: "processwire-lennol-2019"
 		}, {
-			title: "Suomen Taksitarvike Oy",
-			url: "https://taksitarvike.fi/",
-			img: "processwire-taksitarvike-2020"
+			title: "Omamuutto",
+			what: "Verkkokauppa, vuokraus",
+			url: "https://omamuutto.fi/",
+			img: "processwire-omamuutto-2020"
 		}
 	]
 	let references
@@ -63,17 +76,20 @@
 
 {#if references}
 
-<div id="references"><div class="container"><div class="row">
+<div id="references"><div class="container tc"><div id="info"><h1>Toteutettuja projektejamme</h1><p>Haluatko nähdä lisää referenssejä?<br>Ota yhteyttä <a href="mailto:timo@dataline.fi" rel="nofollow">timo@dataline.fi</a>.</p></div><div class="row">
 
 	{#if references == "verkkokaupat"}
 
 		{#each shop as item}
-		<a class="col-4 mb" href={item.url} title={item.title} target="_blank" rel="noopener">
-			<picture>
-				<source srcset={"/images/ref-"+ item.img +".webp"} type="image/webp">
-				<source srcset={"/images/ref-"+ item.img +".jpg"} type="image/jpeg">
-				<img src={"/images/ref-"+ item.img +".jpg"} alt={item.title}>
-			</picture>
+		<a class="col-4 r12 mb" href={item.url} title={item.title} target="_blank" rel="noopener">
+			<figure>
+				<picture>
+					<source srcset={"/images/ref-"+ item.img +".webp"} type="image/webp">
+					<source srcset={"/images/ref-"+ item.img +".jpg"} type="image/jpeg">
+					<img src={"/images/ref-"+ item.img +".jpg"} alt={item.title}>
+				</picture>
+				<figcaption class="caption"><h2>{item.title}</h2><h3>{item.what}</h3></figcaption>
+			</figure>
 		</a>
 		{/each}
 
@@ -90,7 +106,7 @@
 			<div id="sum" class="mb-4">Tarjoamme juuri sinulle parhaat verkkokaupparatkaisut kotimaiseen ja kansainväliseen kaupankäyntiin erittäin kilpailukykyisellä hinnalla ja monen vuoden osaamisella.</div>
 			<div id="buttons">
 				<a class="btn" href="tel:+358407746121" title="Keskutellaan miten voimme parantaa teidän verkkosivustoa" rel="nofollow noopener">Soita meille</a>
-				<a class="btn" href="mailto:myynti@dataline.fi" title="Lähetä rohkeasti sähköpostia ja sovitaan tapaaminen" rel="nofollow noopener">Lähetä viesti</a>
+				<a class="btn" href="mailto:timo@dataline.fi" title="Lähetä rohkeasti sähköpostia ja sovitaan tapaaminen" rel="nofollow noopener">Lähetä viesti</a>
 			</div>
 		</div>
 	</div>
@@ -98,12 +114,12 @@
 
 {#each sections as item}
 <section id={item.id} class={"big bg"+ item.bg}>
-	<div class="container">
+	<div class="container py">
 		<div class="row">
 			<div class="col-4 r1">
 				<picture>
-					<source srcset={"/images/palvelumme-"+ item.id +"-320.webp"} type="image/webp">
-					<source srcset={"/images/palvelumme-"+ item.id +"-320.jpg"} type="image/jpeg">
+					<source srcset={"/images/"+ item.img +"-320.webp"} type="image/webp">
+					<source srcset={"/images/"+ item.img +"-320.jpg"} type="image/jpeg">
 					<img class="img" src={"/images/palvelumme-"+ item.id +"-320.jpg"} alt={item.img_desc}>
 				</picture>
 			</div>
@@ -111,7 +127,16 @@
 				<div class="cell">
 					<h2>{item.title}</h2>
 					{@html item.desc}
-					{#if item.button == 1}<div class="references"><button class="btn" on:click={() => references = item.id}>Referenssit</button></div>{/if}
+					{#if item.button > 0}
+						{#if item.button == 1}
+							<div class="references"><button class="btn" on:click={() => references = item.id}>Referenssit</button></div>
+						{:else}
+							<div id="buttons">
+								<a class="btn" href="tel:+358407746121" title="Mietitään miten voisimme auttaa teitä" rel="nofollow noopener">Soita meille</a>
+								<a class="btn" href="mailto:timo@dataline.fi" title="Lähetä rohkeasti sähköpostia ja sovitaan tapaaminen" rel="nofollow noopener">Lähetä viesti</a>
+							</div>
+						{/if}
+					{/if}
 				</div>
 			</div>
 		</div>
@@ -119,7 +144,7 @@
 </section>
 {/each}
 
-<section id="yhteystiedot" class="big">
+<section id="yhteystiedot" class="big tc">
 	<div class="container">
 		<h2>Yhteystiedot</h2>
 		<div id="yritys" class="rmb">
